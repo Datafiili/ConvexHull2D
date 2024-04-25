69022c972d4cda7effdadf7ee2c2f49b1a280289
Aarni Junkkala <aarni.junkkala@gmail.com>
2024-04-25 15:28:51 +0300

    Update

--- ConvexHull2D.py
+++ ConvexHull2D.py
@@ -27,8 +27,9 @@ def ConvexHull2D(vectors):
             if vectors[i] != Points[-1]:
                 #Calculates the angle
                 Angle = math.atan2(vectors[i][0] - Points[-1][0], vectors[i][1] - Points[-1][1])
-                if Angle > math.pi * 2: #If angle goes below zero, it must be rotated around
-                    Angle -= math.pi * 2
+                Angle -= AngleHolder
+                if Angle < 0:
+                    Angle += math.pi * 2
                 #print(vectors[i], Angle)
                 if Angle < SmallestAngle: #remembers one thatis smallest
                     SmallestAngle = Angle
@@ -44,42 +45,53 @@ def ConvexHullV2(vectors):
     if len(vectors) <= 2:
         return vectors
     
-    corners = [vectors[0],vectors[0],vectors[0],vectors[0]] # Most left, right, top and bottomest vectors
+    corners = [vectors[0],vectors[0],vectors[0],vectors[0]] # Most left, top, right and bottom vectors
     for i in range(1,len(vectors)):
-        if vectors[i][0] < corners[0][0]:
+        if vectors[i][0] < corners[0][0]: #Left
             corners[0] = vectors[i]
-        if vectors[i][1] > corners[1][1]:
+        if vectors[i][1] > corners[1][1]: #Top
             corners[1] = vectors[i]
-        if vectors[i][0] > corners[2][0]:
+        if vectors[i][0] > corners[2][0]: #Right
             corners[2] = vectors[i]
-        if vectors[i][1] < corners[3][1]:
+        if vectors[i][1] < corners[3][1]: #Bottom
             corners[3] = vectors[i]
     print("Corners:",corners)
     
     Packs = [[corners[1]],[[corners[2]]],[[corners[3]]],[[corners[0]]]] #Packs of vertexes that are possibly on the hull. Seperated based on the 4 corners.
+    
     for i in range(len(vectors)):
         if vectors[i] not in corners:
             #Top left pack
             if vectors[i][0] < corners[1][0] and vectors[i][1] > corners[0][1]:
-                Packs[0].append(vectors[i])
+                if math.atan2(vectors[i][0] - corners[0][0],vectors[i][1] - corners[0][1]) > math.atan2(corners[1][0] - corners[0][0],corners[1][1] - corners[0][1]): 
+                    Packs[0].append(vectors[i])
             #Top right
             if vectors[i][0] > corners[1][0] and vectors[i][1] > corners[2][1]:
-                Packs[1].append(vectors[i])
+                if math.atan2(vectors[i][0] - corners[1][0],vectors[i][1] - corners[1][1]) < math.atan2(corners[2][0] - corners[1][0], corners[2][1] - corners[1][1]):
+                    Packs[1].append(vectors[i])
             #Bottom right
             if vectors[i][0] > corners[3][0] and vectors[i][1] < corners[2][1]:
-                Packs[2].append(vectors[i])
+                if math.atan2(vectors[i][0] - corners[2][0], vectors[i][1] - corners[2][1]) > math.atan2(corners[3][0] - corners[2][0], corners[3][1] - corners[2][1]):
+                    Packs[2].append(vectors[i])
             #Bottom left
             if vectors[i][0] < corners[3][0] and vectors[i][1] < corners[0][1]:
-                Packs[3].append(vectors[i])
+                if math.atan2(vectors[i][0] - corners[3][0],vectors[i][1] - corners[3][1]) < math.atan2(corners[0][0] - corners[3][0], corners[0][1] - corners[3][1]):
+                    Packs[3].append(vectors[i])
     for i in range(4):
         print("Pack",i+1,Packs[i])
-        
+    #print("Angle to ", vectors[i] , ": " , math.atan2(corners[0][0] - corners[3][0], corners[0][1] - corners[3][1]))
+
+    #Filter packs based on angle. If angle is greater than from corner to next, then it isn't in the area.
+    points = [corners[0]]
+               
 
 
 if __name__ == "__main__":
     print("##--------------------##")
     #print(ConvexHull2D([[1.2,1.5],[2,0],[0,0],[-2,-2.2],[-3,1],[-1,-4]]))
+    print(ConvexHull2D([[5,1],[-5,-1],[1,5],[-1,-5],[1,1],[2,2],[3,3],[-1,1],[-2,2],[-3,3],[1,-1],[2,-2],[3,-3],[-1,-1],[-2,-2],[-3,-3],[0,0]]))
     print(ConvexHullV2([[5,1],[-5,-1],[1,5],[-1,-5],[1,1],[2,2],[3,3],[-1,1],[-2,2],[-3,3],[1,-1],[2,-2],[3,-3],[-1,-1],[-2,-2],[-3,-3],[0,0]]))
+
 #     import random
 #     import time
 #     tik = time.time()
