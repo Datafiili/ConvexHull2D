63470abbb289eb4d544d02f6a266ef600198a04e
Aarni Junkkala <aarni.junkkala@gmail.com>
2024-05-07 15:12:57 +0300

    Update, still doesn't work

--- ConvexHull2D.py
+++ ConvexHull2D.py
@@ -4,7 +4,7 @@
 import math
 
 def ConvexHull2D(vectors):
-    if len(vectors) <= 2:
+    if len(vectors) <= 3: #Just returns case where no calculations are needed. 
         return vectors
     #Finds the vector that is most left
     Leftest = 0
@@ -18,8 +18,6 @@ def ConvexHull2D(vectors):
     while True:
         SmallestAngle = math.pi * 2 #Defaults to impossible
         SmallestIndex = -1 # -||-
-        #print("Angle holder:", AngleHolder)
-        #print("Starting point:",Points[-1])
         for i in range(len(vectors)): #Checks all angles compared to latest one
             if vectors[i] != Points[-1]:
                 #Calculates the angle
@@ -27,19 +25,17 @@ def ConvexHull2D(vectors):
                 Angle -= AngleHolder
                 if Angle < 0:
                     Angle += math.pi * 2
-                #print(vectors[i], Angle)
                 if Angle < SmallestAngle: #remembers one thatis smallest
                     SmallestAngle = Angle
                     SmallestIndex = i
         if vectors[SmallestIndex] in Points: #If tries to return back, then we are done
             break
         Points.append(vectors[SmallestIndex])
-        #print("Connected to",vectors[SmallestIndex])
         AngleHolder += SmallestAngle
     return Points
 
-def ConvexHullV2(vectors):
-    if len(vectors) <= 2:
+def ConvexHullAarni(vectors):
+    if len(vectors) <= 3: #Just returns case where no calculations are needed. 
         return vectors
     
     corners = [vectors[0],vectors[0],vectors[0],vectors[0]] # Most left, top, right and bottom vectors
@@ -52,10 +48,8 @@ def ConvexHullV2(vectors):
             corners[2] = vectors[i]
         if vectors[i][1] < corners[3][1]: #Bottom
             corners[3] = vectors[i]
-    #print("Corners:",corners)
     
     Packs = [[corners[1]],[corners[2]],[corners[3]],[corners[0]]] #Packs of vertexes that are possibly on the hull. Seperated based on the 4 corners.
-    print("## ----- Calculate Corners ----- ##")
     CornerAngles = [] #Angle from corner to a nother
     for i in range(len(corners)):
         Angle = math.atan2(corners[(i + 1) % len(corners)][0] - corners[i][0],corners[(i + 1) % len(corners)][1] - corners[i][1])
@@ -65,11 +59,9 @@ def ConvexHullV2(vectors):
 
     for i in range(len(vectors)):
         if vectors[i] not in corners:
-            print("Vectors[i]",vectors[i])
             #Top left pack
             if vectors[i][0] < corners[1][0] and vectors[i][1] > corners[0][1]:
                 if math.atan2(vectors[i][0] - corners[0][0],vectors[i][1] - corners[0][1]) < CornerAngles[0]: 
-                    print("Added to top left")
                     Packs[0].append(vectors[i])
             #Top right
             if vectors[i][0] > corners[1][0] and vectors[i][1] > corners[2][1]:
@@ -83,27 +75,22 @@ def ConvexHullV2(vectors):
             if vectors[i][0] < corners[3][0] and vectors[i][1] < corners[0][1]:
                 if math.atan2(vectors[i][0] - corners[3][0],vectors[i][1] - corners[3][1]) < math.atan2(corners[0][0] - corners[3][0], corners[0][1] - corners[3][1]):
                     Packs[3].append(vectors[i])
-    for i in range(4):
-        print("Pack",i+1,Packs[i])
-    print("## ----- Packs ----- ##")
-    #print("Angle to ", vectors[i] , ": " , math.atan2(corners[0][0] - corners[3][0], corners[0][1] - corners[3][1]))
-
     #Filter packs based on angle. If angle is greater than from corner to next, then it isn't in the area.
     points = [corners[0]]
     Index = 0
-    Repeats = 0
+    stopper = 0
     while True:
-        Repeats += 1
-        if Repeats > 20:
-            print("Failed")
+        stopper += 1
+        if stopper > 20:
             break
+        print(points[-1])
         SmallestAngle = math.pi * 2 #Defaults to impossible
         SmallestIndex = -1 
-        print("##-----##")
-        print("From:", points[-1])
         for i in range(len(Packs[Index])):
-            Angle = math.atan2(Packs[Index][i][0] - points[-1][0],Packs[Index][i][1] - points[-1][1])
-            print("To",Packs[Index][i], Angle)
+            if points[-1] == Packs[Index][i]:
+                continue
+            #Calculates the angle. Angle is shown in way that to top is zero, increases clock-wise
+            Angle = (math.atan2(Packs[Index][i][0] - points[-1][0],Packs[Index][i][1] - points[-1][1]) + math.pi * 2) % (math.pi * 2)
             if Index > 1:
                 Angle = abs(Angle) + math.pi
             if Angle < SmallestAngle:
@@ -121,14 +108,30 @@ if __name__ == "__main__":
     #print(ConvexHull2D([[1.2,1.5],[2,0],[0,0],[-2,-2.2],[-3,1],[-1,-4]]))
     #print(ConvexHull2D([[5,1],[-5,-1],[1,5],[-1,-5],[1,1],[2,2],[3,3],[-1,1],[-2,2],[-3,3],[1,-1],[2,-2],[3,-3],[-1,-1],[-2,-2],[-3,-3],[0,0]]))
     #print(ConvexHullV2([[5,1],[-5,-1],[1,5],[-1,-5],[1,1],[2,2],[3,3],[-1,1],[-2,2],[-3,3],[1,-1],[2,-2],[3,-3],[-1,-1],[-2,-2],[-3,-3],[0,0]]))
-    print(ConvexHullV2([[0,3],[3,0],[0,-3],[-3,0],[2,2],[-2,2],[2,-2],[-2,-2],[0,0],[1,1],[-1,1],[1,-1],[-1,-1],[1,0],[0,1],[-1,0],[0,-1]]))
+    #print(ConvexHullV2([[0,3],[3,0],[0,-3],[-3,0],[2,2],[-2,2],[2,-2],[-2,-2],[0,0],[1,1],[-1,1],[1,-1],[-1,-1],[1,0],[0,1],[-1,0],[0,-1]]))
+    #print(ConvexHullAarni([[0,3],[0,2],[0,1],[0,0],[0,-1],[0,-2],[0,-3],[3,0],[-3,0],[-1,1],[-2,1],[-1,2],[-2.5,1.5],[-1.5,2.5]]))
+    print("Generating Data")
+    import random
+    Datasets = []
+    for i in range(1):
+        NewData = []
+        for k in range(100):
+            
 
-#     import random
-#     import time
-#     tik = time.time()
-#     for i in range(2000):
-#         L = []
-#         for i in range(1000):
-#             L.append([random.uniform(-5.0,5.0),random.uniform(-5.0,5.0)])
-#         ConvexHull2D(L)
-#     print(time.time() - tik)
+            NewData.append([random.uniform(-5.0,5.0),random.uniform(-5.0,5.0)])
+            Datasets.append(NewData)
+    
+    print("Data Generated")
+    print("Data:",Datasets)
+    import time
+    tik = time.time()
+
+    for i in range(len(Datasets)):
+        ConvexHull2D(Datasets[i])
+    print("Brute: ",time.time()-tik)
+
+    tik = time.time()
+
+    for i in range(len(Datasets)):
+        ConvexHullAarni(Datasets[i])
+    print("Aarni: ",time.time()-tik)
